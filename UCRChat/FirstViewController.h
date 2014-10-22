d87659ba1756ef332ff2514a7ab40a8fae169906
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2014-10-21 17:41:17 -0700

    first commit

---
+++ UCRChat/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  UCRChat
+//
+//  Created by Gustavo Blanco on 10/21/14.
+//  Copyright (c) 2014 me.gustavob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ UCRChat/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  UCRChat
+//
+//  Created by Gustavo Blanco on 10/21/14.
+//  Copyright (c) 2014 me.gustavob. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
