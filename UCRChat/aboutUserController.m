de30f915861c05ea5aaf8813be45c84cdaeb7992
Fernando Gonzalez <fgonz009@ucr.edu>
2014-11-12 01:34:11 -0800

    Can't add files.

---
+++ UCRChat/aboutUserController.m
@@ -0,0 +1,13 @@
+//
+//  aboutUserController.m
+//  UCRChat
+//
+//  Created by user25108 on 11/12/14.
+//  Copyright (c) 2014 me.gustavob. All rights reserved.
+//
+
+#import "aboutUserController.h"
+
+@implementation aboutUserController
+
+@end
