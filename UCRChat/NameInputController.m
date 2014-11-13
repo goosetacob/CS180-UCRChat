bc8f36150d5d4fcc5a06ccfd24c9b7f2d2958ac1
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2014-11-12 16:05:28 -0800

    sync Fernando and Gus code

--- UCRChat/NameInputController.m
+++ UCRChat/NameInputController.m
@@ -35,18 +35,25 @@
 - (IBAction)backToUserFileController:(UIBarButtonItem *)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
-- (IBAction)saveUserNameInput:(UIBarButtonItem *)sender {
-    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
-    [query getObjectInBackgroundWithId:[[PFUser currentUser] objectId]
-                                 block:^(PFObject *userInfo, NSError *error) {
-        
-        // Now let's update it with some new data.8
-        userInfo[@"fullName"] = self.textField.text;
-        [userInfo saveInBackground];
-        
-        [self dismissViewControllerAnimated:YES completion:nil];
-    }];
+
+-(IBAction)saveReturnToUserFileController:(UIBarButtonItem *)sender{
     
+    PFObject *userPointer = [PFObject objectWithClassName:@"_User"];
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
+    [query getObjectInBackgroundWithId:_currentUserId block:^(PFObject *userPointer, NSError *error) {
+        // Do something with the returned PFObject in the gameScore variable.
+        
+        NSString *playerName = userPointer[@"fullName"];
+        if (![self.textView.text isEqualToString:playerName]){
+            
+            userPointer[@"fullName"] = self.textView.text;
+            [userPointer saveInBackground];
+        }
+       
+        NSLog(@"%@", userPointer);
+    }];
+    [self dismissViewControllerAnimated:YES completion:nil];
     
 }
 
